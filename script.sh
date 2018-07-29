b3e5ecb8b418f882c809f6419254298b57c74ef1
Vladimir Smyshlyaev <v@vforvova.ru>
2018-07-29 21:06:58 +0300

    Add scripts

--- script.sh
+++ script.sh
@@ -1,3 +1,11 @@
 #!/bin/usr/bash
+
+# Atom
 rm $HOME/.atom/config.cson
 ln -s $HOME/dotfiles/.atom/config.cson $HOME/.atom/
+
+rm $HOME/.atom/keymap.cson
+ln -s $HOME/dotfiles/.atom/keymap.cson $HOME/.atom/
+
+rm $HOME/.atom/snippets.cson
+ln -s $HOME/dotfiles/.atom/snippets.cson $HOME/.atom/
