8aab5a77a2f70cac4791e6019e54d088be275885
Vladimir Smyshliaev <vladimirs@unity3d.com>
2018-09-26 14:10:09 +0300

    Add fish configurations

---
+++ fish/.config/fish/abbreviations/git.fish
@@ -0,0 +1,14 @@
+abbr -a g git
+abbr -a ga git add
+abbr -a gc git commit
+abbr -a gco git checkout
+abbr -a gd git diff
+abbr -a gl git pull
+abbr -a gp git push
+abbr -a gm git merge
+abbr -a greb git rebase
+abbr -a gres git reset
+abbr -a gs git status
+abbr -a gb git branch
+abbr -a gpr git pull-request -o
+
