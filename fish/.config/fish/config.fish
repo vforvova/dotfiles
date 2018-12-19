0d1ef5dde33c98eef1d245079c93301680dd1f67
Vladimir Smyshliaev <vladimirs@unity3d.com>
2018-12-19 07:31:29 +0200

    Add variables to Fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,20 @@
 set -g EDITOR 'vim'
 set -g LC_ALL 'en_US.UTF-8'
 
-set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
+# N
+if type -q n
+  set -x N_PREFIX $HOME/n
+  add_to_path $N_PREFIX/bin
+end
+
+# Golang
+if type -q go
+  set -x GOPATH $HOME/Projects/.go
+  add_to_path $GOPATH/bin
+  add_to_path /usr/local/opt/go/libexec/bin
+end
+
+add_to_path "/usr/local/opt/gettext/bin"
 
 if not set -q fish_user_abbreviations
   source ./abbreviations.fish
