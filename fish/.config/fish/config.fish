7e775f3c2ab3adc427a031925ff6180fe67e802b
Vladimir Smyshliaev <vladimirs@unity3d.com>
2019-05-05 19:57:12 +0300

    Fix fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,13 @@ set -x EDITOR 'vim'
 set -x LANG 'en_US.UTF-8'
 set -x LC_ALL 'en_US.UTF-8'
 
+#  Fisher autoinstallation
+if not functions -q fisher
+  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+  fish -c fisher
+end
+
 # N
 if type -q n
   set -x N_PREFIX $HOME/n
@@ -24,5 +31,5 @@ end
 add_to_path "/usr/local/opt/gettext/bin"
 
 if not set -q fish_user_abbreviations
-  source ./abbreviations.fish
+  source $HOME/.config/fish/abbreviations.fish
 end
