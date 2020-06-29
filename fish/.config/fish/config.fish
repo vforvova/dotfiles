442a98c26c61a77df5eeddff9e3cc1d78e00c251
Vladimir Smyshliaev <vladimirs@unity3d.com>
2020-06-29 21:36:40 +0300

    Fix paths for tmux

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,53 +10,34 @@ if not functions -q fisher
 end
 
 
-if not set -q IS_INSTALLED
-  add_to_path "/usr/local/bin"
-  add_to_path "/usr/local/opt/gettext/bin"
-  set -U IS_INSTALLED true
-end
+add_to_path "/usr/local/bin"
+add_to_path "/usr/local/opt/gettext/bin"
 
 # N
 if type -q n
   set -x N_PREFIX $HOME/n
-
-  if not set -q N_IS_INSTALLED
-    add_to_path $N_PREFIX/bin
-    set -U N_IS_INSTALLED true
-  end
+  add_to_path $N_PREFIX/bin
 end
 
 # Node
 if type -q node
   set -x NPM_PACKAGES $HOME/.npm-packages
-
-  if not set -q NODE_IS_INSTALLED
-    add_to_path $NODE_PACKAGES/bin
-    set -U NODE_IS_INSTALLED true
-  end
+  add_to_path $NODE_PACKAGES/bin
 end
 
 # Golang
 if type -q go
   set -x GO111MODULE on
   set -x GOPATH $HOME/Projects/.go
-
-  if not set -q GO_IS_INSTALLED
-    add_to_path $GOPATH/bin
-    add_to_path /usr/local/opt/go/libexec/bin
-    set -U GO_IS_INSTALLED true
-  end   
+  add_to_path $GOPATH/bin
+  add_to_path /usr/local/opt/go/libexec/bin
 end
 
 # Rust
 if type -q go
   set -x CARGO_HOME $HOME/.cargo
   set -x RUSTUP_HOME $HOME/.rustup
-
-  if not set -q RUST_IS_INSTALLED
-    add_to_path $CARGO_HOME/bin
-    set -U RUST_IS_INSTALLED true
-  end   
+  add_to_path $CARGO_HOME/bin
 end
 
 if not set -q fish_user_abbreviations
