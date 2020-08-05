a2ea20994faa1e9f9e0f1e9e6a894dd6f4cd142c
Vladimir Smyshliaev <vladimirs@unity3d.com>
2020-08-05 22:17:09 +0300

    Change fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,37 +9,6 @@ if not functions -q fisher
   fish -c fisher
 end
 
-
-add_to_path "/usr/local/bin"
-add_to_path "/usr/local/opt/gettext/bin"
-
-# N
-if type -q n
-  set -x N_PREFIX $HOME/n
-  add_to_path $N_PREFIX/bin
-end
-
-# Node
-if type -q node
-  set -x NPM_PACKAGES $HOME/.npm-packages
-  add_to_path $NODE_PACKAGES/bin
-end
-
-# Golang
-if type -q go
-  set -x GO111MODULE on
-  set -x GOPATH $HOME/Projects/.go
-  add_to_path $GOPATH/bin
-  add_to_path /usr/local/opt/go/libexec/bin
-end
-
-# Rust
-if type -q go
-  set -x CARGO_HOME $HOME/.cargo
-  set -x RUSTUP_HOME $HOME/.rustup
-  add_to_path $CARGO_HOME/bin
-end
-
 if not set -q fish_user_abbreviations
   source $HOME/.config/fish/abbreviations.fish
 end
--- fish/.config/fish/functions/add_to_path.fish
+++ fish/.config/fish/functions/add_to_path.fish
@@ -4,3 +4,35 @@ function add_to_path --description "Persistently prepends to your PATH via fish_
     set --universal fish_user_paths $argv $fish_user_paths
   end
 end
+
+# Examples
+#
+# add_to_path "/usr/local/bin"
+# add_to_path "/usr/local/opt/gettext/bin"
+#
+# N ==========================================
+# if type -q n
+#   set -x N_PREFIX $HOME/n
+#   add_to_path $N_PREFIX/bin
+# end
+#
+# Node =======================================
+# if type -q node
+#   set -x NPM_PACKAGES $HOME/.npm-packages
+#   add_to_path $NODE_PACKAGES/bin
+# end
+#
+# Golang =====================================
+# if type -q go
+#   set -x GO111MODULE on
+#   set -x GOPATH $HOME/Projects/.go
+#   add_to_path $GOPATH/bin
+#   add_to_path /usr/local/opt/go/libexec/bin
+# end
+#
+# Rust =======================================
+# if type -q go
+#   set -x CARGO_HOME $HOME/.cargo
+#   set -x RUSTUP_HOME $HOME/.rustup
+#   add_to_path $CARGO_HOME/bin
+# end
